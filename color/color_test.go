9ba9b951b4cadf3d410d8d1635906a0cc459240e
vompressor <vompressor@gmail.com>
2021-07-23 14:30:52 +0000

    t

--- color/color_test.go
+++ color/color_test.go
@@ -3,10 +3,10 @@ package color_test
 import (
 	"testing"
 
-	"github.com/vompressor/vfmlib/vfmlib/color"
-	"github.com/vompressor/vfmlib/vfmlib/color/background_color"
-	"github.com/vompressor/vfmlib/vfmlib/color/basic"
-	"github.com/vompressor/vfmlib/vfmlib/color/text_color"
+	"github.com/vompressor/vfmlib/color"
+	"github.com/vompressor/vfmlib/color/background_color"
+	"github.com/vompressor/vfmlib/color/basic"
+	"github.com/vompressor/vfmlib/color/text_color"
 )
 
 func TestColor(t *testing.T) {
